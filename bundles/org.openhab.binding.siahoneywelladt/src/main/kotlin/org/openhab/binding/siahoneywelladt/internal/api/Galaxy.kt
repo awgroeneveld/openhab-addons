98be396cb92910d7b085e2ff2105d50bd06a17e3
A.W. Groeneveld <awgroeneveld@logibar.com>
2020-08-19 14:38:45 +0200

    should now handle all incoming OK

--- bundles/org.openhab.binding.siahoneywelladt/src/main/kotlin/org/openhab/binding/siahoneywelladt/internal/api/Galaxy.kt
+++ bundles/org.openhab.binding.siahoneywelladt/src/main/kotlin/org/openhab/binding/siahoneywelladt/internal/api/Galaxy.kt
@@ -1,31 +1,33 @@
 package org.openhab.binding.siahoneywelladt.internal.api
 
+import org.openhab.binding.siahoneywelladt.internal.model.Area
 import org.openhab.binding.siahoneywelladt.internal.model.AreaAction
 import org.openhab.binding.siahoneywelladt.internal.model.AreaAlarmState
 import org.openhab.binding.siahoneywelladt.internal.model.AreaArmedState
 import org.openhab.binding.siahoneywelladt.internal.model.AreaReadyState
 import org.openhab.binding.siahoneywelladt.internal.model.OutputState
+import org.openhab.binding.siahoneywelladt.internal.model.SingleZoneTechnicalState
+import org.openhab.binding.siahoneywelladt.internal.model.Zone
 import org.openhab.binding.siahoneywelladt.internal.model.ZoneArmingState
-import org.openhab.binding.siahoneywelladt.internal.model.ZoneTechnicalState
 
 
 interface Galaxy {
     fun isValidZoneNumber(zoneId: Int)
     fun isValidOutputNumber(outputId: Int)
 
-    fun performAreaAction(action: AreaAction, blockNumber: Int): Boolean
-    fun performAreaActionOnAll(action: AreaAction) = performAreaAction(action, 0)
-    fun getArmedState(blockNumber: Int): AreaArmedState
-    fun getArmedStateByArea(): Map<Int, AreaArmedState>
-    fun getAlarmStateByArea(): Map<Int, AreaAlarmState>
-    fun getReadyStateByArea(): Map<Int, AreaReadyState>
-    fun setZoneArmingState(zoneId: Int, state: ZoneArmingState): Boolean
-    fun getZoneArmingState(zoneId: Int): ZoneArmingState
-    fun getZoneArmingStatesByZone()
-    fun getZoneState(zoneId: Int, state: ZoneTechnicalState)
-    fun getAllZonesWithState(state: ZoneTechnicalState): List<Int>
+    fun performAreaAction(action: AreaAction, area: Area): Boolean
+    fun performAreaActionOnAll(action: AreaAction)
+    fun getArmedState(area: Area): AreaArmedState
+    fun getArmedStateByArea(): Map<Area, AreaArmedState>
+    fun getAlarmStateByArea(): Map<Area, AreaAlarmState>
+    fun getReadyStateByArea(): Map<Area, AreaReadyState>
+    fun setZoneArmingState(zone: Zone, state: ZoneArmingState): Boolean
+    fun getZoneArmingState(zone: Zone): ZoneArmingState
+    fun getZoneArmingStatesByZone(): Map<Zone, ZoneArmingState>
+    fun getZoneState(zone: Zone): SingleZoneTechnicalState
+    fun getAllZonesWithState(state: SingleZoneTechnicalState): List<Zone>
     fun getAllOutputStatesByOutput(): Map<Int, OutputState>
     fun setOutputState(outputId: Int, state: OutputState): Boolean
-    fun setAllOutputStates(state: OutputState): Boolean = setOutputState(0, state)
+    fun setAllOutputStates(state: OutputState): Boolean
 
 }
--- bundles/org.openhab.binding.siahoneywelladt/src/main/kotlin/org/openhab/binding/siahoneywelladt/internal/model/command/AllAreasStateDecoder.kt
+++ bundles/org.openhab.binding.siahoneywelladt/src/main/kotlin/org/openhab/binding/siahoneywelladt/internal/model/command/AllAreasStateDecoder.kt
@@ -1,12 +1,9 @@
 package org.openhab.binding.siahoneywelladt.internal.model.command
 
 import org.openhab.binding.siahoneywelladt.internal.model.Area
-import org.openhab.binding.siahoneywelladt.internal.model.Rio
 import org.openhab.binding.siahoneywelladt.internal.model.SiaAreaConfigurationEvent
 import org.openhab.binding.siahoneywelladt.internal.model.SiaBlock
 import org.openhab.binding.siahoneywelladt.internal.model.SiaConfigurationEvent
-import org.openhab.binding.siahoneywelladt.internal.model.SiaZoneConfigurationEvent
-import org.openhab.binding.siahoneywelladt.internal.model.Zone
 
 class AllAreasStateDecoder<X : Enum<*>>(
     private val toEnum: (Int) -> X?
@@ -34,47 +31,3 @@ class AllAreasStateDecoder<X : Enum<*>>(
 
 }
 
-class AllZonesStateDecoder<X : Enum<*>>(private val toEnum: (Int) -> X?) : SiaStateRequestDecoder {
-
-    fun byteToZoneMessage(byteNumber: Int, value: Int): List<Pair<Zone, X>> {
-        val rio = byteToRio(byteNumber)
-        return rio?.zones?.mapIndexed { index, it -> it to toEnum((value shr index) % 2)!! }
-            ?: listOf()
-    }
-
-    fun byteToRio(byteNumber: Int): Rio? =
-        when (byteNumber) {
-            0 -> Rio(1, 0)
-            1 -> Rio(1, 1)
-            in 4..33 -> {
-                val rioSequence = byteNumber - 2
-                Rio(1 + rioSequence / 16, rioSequence % 16)
-            }
-            in 35..66 -> {
-                val rioSequence = byteNumber - 3
-                Rio(1 + rioSequence / 16, rioSequence % 16)
-            }
-            else -> null
-        }
-
-    override fun decode(siaBlock: SiaBlock, siaStateRequestType: SiaStateRequestType): SiaConfigurationEvent? {
-        val messageNumber = siaBlock.messageAsString.substring(
-            siaStateRequestType.messageHeaderLength - 2,
-            siaStateRequestType.messageHeaderLength - 1
-        )
-            .toInt()
-        val byteOffset = (messageNumber - 1) * 35
-        val zoneStateAssignments = siaBlock.message.drop(siaStateRequestType.messageHeaderLength)
-            .mapIndexed { index, byte -> byteToZoneMessage(index + byteOffset, byte.toInt() and 0xff) }
-            .flatten()
-            .toMap()
-        return SiaZoneConfigurationEvent(
-            siaStateRequestType,
-            zoneStateAssignments,
-            siaBlock.message,
-            siaBlock.needsAcknowledge(),
-            siaBlock.getTotalLength()
-        )
-    }
-
-}
